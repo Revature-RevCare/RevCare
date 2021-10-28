7b1730ed932c1ef0263452977d4a78e25d0fca7f
tungtt0806 <tung.tt0806@gmail.com>
2021-10-28 15:24:08 -0400

    Diagnos Controller

--- Spring/src/main/java/com/revature/Spring/controllers/PatientDiagnosisController.java
+++ Spring/src/main/java/com/revature/Spring/controllers/PatientDiagnosisController.java
@@ -1,6 +1,8 @@
 package com.revature.Spring.controllers;
 
 import com.revature.Spring.services.PatientDiagnosisService;
+import com.revature.Spring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/diagnosis")
 public class PatientDiagnosisController {
     private final PatientDiagnosisService patientDiagnosisService;
+    private final UserService userService;
 
-    public PatientDiagnosisController(PatientDiagnosisService patientDiagnosisService) {
+    public PatientDiagnosisController(PatientDiagnosisService patientDiagnosisService, UserService userService) {
         this.patientDiagnosisService = patientDiagnosisService;
+        this.userService = userService;
     }
+
+
 }
