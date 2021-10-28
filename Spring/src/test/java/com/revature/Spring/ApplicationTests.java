c057dfeb16548ea7b24f49169c68332b6efe1176
syonkeu <sylvestre.yonkeu@revature.com>
2021-10-28 13:07:05 -0600

    Upstream test with mockito

--- Spring/src/test/java/com/revature/Spring/ApplicationTests.java
+++ Spring/src/test/java/com/revature/Spring/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.revature.Spring;
 
+import com.revature.Spring.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,8 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	private UserService userService;
+
 
 }
