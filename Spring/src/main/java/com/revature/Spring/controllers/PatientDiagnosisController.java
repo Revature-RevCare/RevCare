461797599590a07428548f5905115402c1cbae8a
tungtt0806 <tung.tt0806@gmail.com>
2021-10-28 20:59:20 -0400

    Adding some basic controllers

--- Spring/src/main/java/com/revature/Spring/controllers/PatientDiagnosisController.java
+++ Spring/src/main/java/com/revature/Spring/controllers/PatientDiagnosisController.java
@@ -1,20 +1,36 @@
 package com.revature.Spring.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.Spring.models.PatientDiagnosis;
 import com.revature.Spring.services.PatientDiagnosisService;
 import com.revature.Spring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/diagnosis")
 public class PatientDiagnosisController {
     private final PatientDiagnosisService patientDiagnosisService;
-    private final UserService userService;
 
-    public PatientDiagnosisController(PatientDiagnosisService patientDiagnosisService, UserService userService) {
+    public PatientDiagnosisController(PatientDiagnosisService patientDiagnosisService) {
         this.patientDiagnosisService = patientDiagnosisService;
-        this.userService = userService;
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<PatientDiagnosis>> getActiveDiagnosis() {
+        List<PatientDiagnosis> patientDiagnoses = patientDiagnosisService.findAllActiveDiagnosis();
+        return new ResponseEntity<>(patientDiagnoses, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/find/{id}")
+    public ResponseEntity<PatientDiagnosis> getDiagnosisById(@PathVariable("id") Integer id) throws JsonProcessingException {
+        PatientDiagnosis pd = patientDiagnosisService.getById(id);
+        return new ResponseEntity<>(pd, HttpStatus.OK);
     }
 
 
