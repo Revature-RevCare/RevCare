1c3c19fa71bf84554f5a6c221c959485192b8eaa
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-10-28 20:27:38 -0500

    .gitignore is now working

--- Spring/src/main/java/com/revature/Spring/repositories/PatientDiagnosisRepo.java
+++
@@ -1,7 +0,0 @@
-package com.revature.Spring.repositories;
-
-import com.revature.Spring.models.PatientDiagnosis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientDiagnosisRepo extends JpaRepository<PatientDiagnosis, Integer> {
-}
