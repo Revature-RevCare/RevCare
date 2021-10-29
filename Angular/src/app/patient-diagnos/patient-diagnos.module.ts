2502c8f80b2f602cebd55f486e6bca9e47678197
tungtt0806 <tung.tt0806@gmail.com>
2021-10-29 12:10:33 -0400

    adding modules

--- Angular/src/app/patient-diagnos/patient-diagnos.module.ts
+++ Angular/src/app/patient-diagnos/patient-diagnos.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OldActiveComponent } from './components/old-active/old-active.component';
 import { OldActiveCaseComponent } from './components/old-active-case/old-active-case.component';
 import { PatientFormComponent } from './components/patient-form/patient-form.component';
 import { DoctorFormComponent } from './components/doctor-form/doctor-form.component';
@@ -9,7 +8,6 @@ import { DoctorFormComponent } from './components/doctor-form/doctor-form.compon
 
 @NgModule({
   declarations: [
-    OldActiveComponent,
     OldActiveCaseComponent,
     PatientFormComponent,
     DoctorFormComponent
