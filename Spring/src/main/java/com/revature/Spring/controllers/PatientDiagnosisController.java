d09471c710d1924a4d280ce526e774900d403bd7
DistractedBranches <damolajao@gmail.com>
2021-10-28 21:03:00 -0400

    My controller parts

--- Spring/src/main/java/com/revature/Spring/controllers/PatientDiagnosisController.java
+++ Spring/src/main/java/com/revature/Spring/controllers/PatientDiagnosisController.java
@@ -1,7 +1,10 @@
 package com.revature.Spring.controllers;
 
+<<<<<<< Updated upstream
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+=======
+>>>>>>> Stashed changes
 import com.revature.Spring.models.PatientDiagnosis;
 import com.revature.Spring.services.PatientDiagnosisService;
 import com.revature.Spring.services.UserService;
@@ -17,7 +20,12 @@ import java.util.List;
 public class PatientDiagnosisController {
     private final PatientDiagnosisService patientDiagnosisService;
 
+<<<<<<< Updated upstream
     public PatientDiagnosisController(PatientDiagnosisService patientDiagnosisService) {
+=======
+    @Autowired
+    public PatientDiagnosisController(PatientDiagnosisService patientDiagnosisService, UserService userService) {
+>>>>>>> Stashed changes
         this.patientDiagnosisService = patientDiagnosisService;
     }
 
@@ -33,5 +41,28 @@ public class PatientDiagnosisController {
         return new ResponseEntity<>(pd, HttpStatus.OK);
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<PatientDiagnosis>> getAllDiagnosises () {
+        List<PatientDiagnosis> pokemons = patientDiagnosisService.findAllDiagnosis();
+        return new ResponseEntity<>(pokemons, HttpStatus.OK);
+    }
+
+    @GetMapping("/active/")
+    public ResponseEntity<List<PatientDiagnosis>> getAllActiveDiagnosises () {
+        List<PatientDiagnosis> patients = patientDiagnosisService.findAllActiveDiagnosis();
+        return new ResponseEntity<>(patients, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<PatientDiagnosis> getPatientById (@PathVariable("id") Integer id) {
+        PatientDiagnosis pokemon = patientDiagnosisService.getById(id);
+        return new ResponseEntity<>(pokemon, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<PatientDiagnosis> addPatient(@RequestBody PatientDiagnosis patient) {
+        PatientDiagnosis newPatient = patientDiagnosisService.addNewDiagnosis(patient);
+        return new ResponseEntity<>(newPatient, HttpStatus.CREATED);
+    }
 
 }
