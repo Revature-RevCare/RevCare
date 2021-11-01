a6c2ea14d3c5bbda75e95f0796b5d74b8c525ee7
AnthonyVu0504 <89420503+AnthonyVu0504@users.noreply.github.com>
2021-11-01 16:19:12 -0500

    Reupdate Module

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,34 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NursesComponent } from './components/nurses/nurses.component';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 import { DiagnosisComponent } from './components/diagnosis/diagnosis.component';
 import { CovidComponent } from './components/covid/covid.component';
-=======
 import { DoctorFormComponent } from './patient-diagnos/components/doctor-form/doctor-form.component';
 import { OldActiveCaseComponent } from './patient-diagnos/components/old-active-case/old-active-case.component';
 import { PatientFormComponent } from './patient-diagnos/components/patient-form/patient-form.component';
->>>>>>> 6cf58c2a470e22069ca504d7f4e1ee00981da2d2
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
     RegisterComponent,
     NursesComponent,
     DoctorsComponent,
     DiagnosisComponent,
-    CovidComponent
-=======
+    CovidComponent,
     DoctorFormComponent,
     OldActiveCaseComponent,
     PatientFormComponent
->>>>>>> 6cf58c2a470e22069ca504d7f4e1ee00981da2d2
   ],
   imports: [
     BrowserModule,
