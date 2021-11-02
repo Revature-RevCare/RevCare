7f8ad9f01bf0753a12e990b7200d1693840db4d5
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-11-02 10:52:12 -0500

    angular model

---
+++ Angular/src/app/models/userModel.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id : number;
+    first_name: string;
+    last_name: string;
+    title: string;
+    email: string;
+    password: string;
+    phone_number: string;
+}
