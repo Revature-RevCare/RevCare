f295c4f8799d3b6a9bfe17908ff30ca8da435542
Xerast <scarnati.james@gmail.com>
2021-11-03 15:49:54 -0500

    deleting unnessary stuff

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -9,10 +9,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { NursesComponent } from './components/nurses/nurses.component';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 import { DiagnosisComponent } from './components/diagnosis/diagnosis.component';
-import { CovidComponent } from './components/covid/covid.component';
 import { DoctorFormComponent } from './components/doctor-form/doctor-form.component';
 import { OldActiveCaseComponent } from './components/old-active-case/old-active-case.component';
-import { CovidVerificationComponent } from './components/covidVerification/covid-verification.component';
 import { CookieService } from 'ngx-cookie-service';
 
 
@@ -24,10 +22,8 @@ import { CookieService } from 'ngx-cookie-service';
     NursesComponent,
     DoctorsComponent,
     DiagnosisComponent,
-    CovidComponent,
     DoctorFormComponent,
-    OldActiveCaseComponent,
-    CovidVerificationComponent
+    OldActiveCaseComponent
   ],
   imports: [
     BrowserModule,
