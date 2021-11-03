5f6557747138564d6ef7a4bcdbb1ecb23c1cc87a
Andy Xue <xueandy32@gmail.com>
2021-11-03 13:55:58 -0500

    Angular login front end

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -9,12 +9,19 @@ import { NursesComponent } from './components/nurses/nurses.component';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 import { DiagnosisComponent } from './components/diagnosis/diagnosis.component';
 import { CovidComponent } from './components/covid/covid.component';
-<<<<<<< HEAD
-=======
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BoardAdminComponent } from './components/board-admin/board-admin.component';
+import { BoardModeratorComponent } from './components/board-moderator/board-moderator.component';
+import { BoardUserComponent } from './components/board-user/board-user.component';
 import { DoctorFormComponent } from './patient-diagnos/components/doctor-form/doctor-form.component';
 import { OldActiveCaseComponent } from './patient-diagnos/components/old-active-case/old-active-case.component';
 import { PatientFormComponent } from './patient-diagnos/components/patient-form/patient-form.component';
->>>>>>> doctor_profile
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+import { authInterceptorProviders } from './helpers/auth.interceptor';
+
+
 
 @NgModule({
   declarations: [
@@ -24,20 +31,23 @@ import { PatientFormComponent } from './patient-diagnos/components/patient-form/
     NursesComponent,
     DoctorsComponent,
     DiagnosisComponent,
-<<<<<<< HEAD
-    CovidComponent
-=======
     CovidComponent,
     DoctorFormComponent,
     OldActiveCaseComponent,
-    PatientFormComponent
->>>>>>> doctor_profile
+    PatientFormComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
