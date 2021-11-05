aadd2b202c0568923d5a4181ebdff02070e56954
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-11-05 10:40:22 -0500

    fixed daig backend

--- Spring/src/main/java/com/revature/Spring/security/AuthTokenFilter.java
+++ Spring/src/main/java/com/revature/Spring/security/AuthTokenFilter.java
@@ -45,6 +45,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+                System.out.println("test" + username);
             }
         } catch (Exception e) {
             logger.error("Cannot set user authentication: {}", e);
