bc45ddb8c7e3a0da9a42221fb8573c25d3ff246a
Xerast <scarnati.james@gmail.com>
2021-11-10 17:34:21 -0600

    updating spring cors

--- Spring/src/main/java/com/revature/Spring/Application.java
+++ Spring/src/main/java/com/revature/Spring/Application.java
@@ -28,7 +28,7 @@ public class Application {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://revature-revcare.s3-website-us-east-1.amazonaws.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
